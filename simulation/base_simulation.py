d4af1d32b6ad1012b92a8cf35ea0cddd34e86707
Vincent Lachapelle <vlachapelle01@gmail.com>
2025-03-03 16:06:48 -0500

    Implemented the save_env_history_file method.

--- simulation/base_simulation.py
+++ simulation/base_simulation.py
@@ -4,9 +4,14 @@ Base classes module.
 The classes defined here should not directly be used as Simulation object.
 """
 
+import json
 import numpy as np
+import os
+from datetime import datetime
 
 from simulation.elements.base_element import Element
+
+RESULT_PATH_DIR = os.path.join("results")
 
 class Simulation():
     """Base class for all Simulation objects."""
@@ -16,8 +21,10 @@ class Simulation():
     elements: list[Element]
     generator: np.random.Generator
     env_history: list[tuple[str]]
+    simulation_name: str
+    simulation_dir: str
     
-    def __init__(self, height: int, width: int, frequency: int, elements: list[Element], generator: np.random.Generator | None = None):
+    def __init__(self, height: int, width: int, frequency: int, elements: list[Element], generator: np.random.Generator | None = None, simulation_name: str | None = None):
         """Base class for all Simulation objects."""
         if generator is not None:
             if not isinstance(generator, np.random.Generator):
@@ -32,7 +39,17 @@ class Simulation():
             self.generator = generator
         else:
             self.generator = np.random.default_rng()
+        if simulation_name is not None:
+            self.simulation_name = str(simulation_name)
+        else:
+            self.simulation_name = f"{self.__class__.__name__}_{datetime.now().strftime("%b/%m/%Y_%H:%M")}"
 
+        self.simulation_dir = os.path.join(RESULT_PATH_DIR, self.simulation_name)
+        try:
+            os.makedirs(self.simulation_dir, exist_ok=False)
+        except OSError:
+            raise ValueError(f"Invalid simulation name. Directory '{self.simulation_dir}' already exists.")
+        
         self.env_history = [(repr(element.shape) for element in self.elements)]
         self.env_history.append((repr(element.shape.center) for element in self.elements))
 
@@ -42,4 +59,8 @@ class Simulation():
             element.update()
         self.env_history.append((repr(element.shape.center) for element in self.elements))
 
-  
+    def save_env_history_file(self):
+        """Saves the simulation's environnement history as a json file."""
+        env_history_file_path = os.path.join(self.simulation_dir, "env_history.json")
+        with open(env_history_file_path, "w") as env_history_file:
+            json.dump(self.env_history, env_history_file)
