1fc5b8000edf9888ebb4be086fcd97ff1b324fbf
Vincent Lachapelle <vlachapelle01@gmail.com>
2025-03-04 15:21:36 -0500

    Implemented __repr__ and __str__ to the base_simulation class.

--- simulation/base_simulation.py
+++ simulation/base_simulation.py
@@ -20,9 +20,9 @@ class Simulation():
     frequency: int
     elements: list[Element]
     generator: np.random.Generator
-    env_history: list[tuple[str]]
     simulation_name: str
-    simulation_dir: str
+    _env_history: list[tuple[str]]
+    _simulation_dir: str
     
     def __init__(self, height: int, width: int, frequency: int, elements: list[Element], generator: np.random.Generator | None = None, simulation_name: str | None = None):
         """Base class for all Simulation objects."""
@@ -44,22 +44,31 @@ class Simulation():
         else:
             self.simulation_name = f"{self.__class__.__name__}_{datetime.now().strftime('%d-%m-%Y_%H:%M')}"
 
-        self.simulation_dir = os.path.join(RESULT_PATH_DIR, self.simulation_name)
-        os.makedirs(self.simulation_dir, exist_ok=True)
+        self._simulation_dir = os.path.join(RESULT_PATH_DIR, self.simulation_name)
+        os.makedirs(self._simulation_dir, exist_ok=True)
         
-        self.env_history = [tuple(element.shape for element in self.elements)]
-        self.env_history.append(tuple(element.shape.center for element in self.elements))
+        self._env_history = [tuple(element.shape for element in self.elements)]
+        self._env_history.append(tuple(element.shape.center for element in self.elements))
+
+    def __repr__(self) -> str:
+        """Object's representation for testing purposes."""
+        return f"{self.__class__.__name__}({self.__dict__})"
+    
+    def __str__(self) -> str:
+        """Object's string representation, excluding hidden attributes."""
+        filtered_attributes = {key: value for key, value in self.__dict__.items() if not key.startswith('_')}
+        return f"{self.__class__.__name__}({', '.join(f'{key}={value}' for key, value in filtered_attributes.items())})"
 
     def step(self) -> None:
         """Updates the states of all of the simulation's elements based on its previous states, then resolves elements interaction."""
         for element in self.elements:
             element.update()
-        self.env_history.append(tuple(element.shape.center for element in self.elements))
+        self._env_history.append(tuple(element.shape.center for element in self.elements))
 
     def save_env_history_file(self):
         """Saves the simulation's environnement history as a json file."""
-        env_history_file_path = os.path.join(self.simulation_dir, "env_history.json")
-        repr_env_history = [[str(element) for element in elements] for elements in self.env_history]
+        env_history_file_path = os.path.join(self._simulation_dir, "env_history.json")
+        repr_env_history = [[str(element) for element in elements] for elements in self._env_history]
         with open(env_history_file_path, "w") as env_history_file:
             json.dump(repr_env_history, env_history_file)
 
