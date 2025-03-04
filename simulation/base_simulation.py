92fefcc6b0bbb25f0e0951b3f9501edf11d85c4a
Vincent Lachapelle <vlachapelle01@gmail.com>
2025-03-04 13:57:34 -0500

    Corrected code so that all unittest are successful.

--- simulation/base_simulation.py
+++ simulation/base_simulation.py
@@ -42,25 +42,26 @@ class Simulation():
         if simulation_name is not None:
             self.simulation_name = str(simulation_name)
         else:
-            self.simulation_name = f"{self.__class__.__name__}_{datetime.now().strftime("%b/%m/%Y_%H:%M")}"
+            self.simulation_name = f"{self.__class__.__name__}_{datetime.now().strftime('%d-%m-%Y_%H:%M')}"
 
         self.simulation_dir = os.path.join(RESULT_PATH_DIR, self.simulation_name)
-        try:
-            os.makedirs(self.simulation_dir, exist_ok=False)
-        except OSError:
-            raise ValueError(f"Invalid simulation name. Directory '{self.simulation_dir}' already exists.")
+        os.makedirs(self.simulation_dir, exist_ok=True)
         
-        self.env_history = [(repr(element.shape) for element in self.elements)]
-        self.env_history.append((repr(element.shape.center) for element in self.elements))
+        self.env_history = [tuple(element.shape for element in self.elements)]
+        self.env_history.append(tuple(element.shape.center for element in self.elements))
 
     def step(self) -> None:
         """Updates the states of all of the simulation's elements based on its previous states, then resolves elements interaction."""
         for element in self.elements:
             element.update()
-        self.env_history.append((repr(element.shape.center) for element in self.elements))
+        self.env_history.append(tuple(element.shape.center for element in self.elements))
 
     def save_env_history_file(self):
         """Saves the simulation's environnement history as a json file."""
         env_history_file_path = os.path.join(self.simulation_dir, "env_history.json")
+        repr_env_history = [[str(element) for element in elements] for elements in self.env_history]
         with open(env_history_file_path, "w") as env_history_file:
-            json.dump(self.env_history, env_history_file)
+            json.dump(repr_env_history, env_history_file)
+
+    def save_config_file(self):
+        pass
